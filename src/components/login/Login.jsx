169592c1bd1d13681bb0bfd8a4d886f9d37c5aa9
karynnemoreira <karynnemoreira@yahoo.com.br>
2025-02-10 19:27:11 -0300

    Finalização da tela de login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,13 @@
-import Capa from "../../assets/Tela Login/imagem tela de login.png"
 import Logo from "../../assets/Tela Login/logo-ws.png"
+import styles from "./Login.module.scss" 
+
 function Login() {
   return (
     <main>
-      <section>
-        <p>Reservado para a imagem</p>
+      <section className={styles.containerImagem}>        
       </section>
 
-      <section>
+      <section className={styles.containerForm}>
         <img src={Logo} alt="Logo da Wilson sons" />
         <h1>Boas vindas ao Novo Portal SISPAR</h1>
         <p>Sistema de Emissão de Boletos e Parcelamento</p>
@@ -19,11 +19,15 @@ function Login() {
           <a href="">Esqueci minha senha</a>
 
           <div>
-            <button>Entrar</button>
-            <button>Criar conta</button>
+            <button className={styles.buttonEntrar}>Entrar</button>
+            <button className={styles.buttonCriar}>Criar conta</button>
           </div>
+
         </form>
+
+
       </section>
+
     </main>
   );
 }
